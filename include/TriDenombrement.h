f4deaa685efde837c4456c685409d5e26f8a19c6
jtrognon <julietrognon74@gmail.com>
2025-11-27 10:26:33 +0100

    fix: Makefile + main + renommage fichiers

---
+++ include/TriDenombrement.h
@@ -0,0 +1,6 @@
+#ifndef TRI_DENOMBREMENT_H
+#define TRI_DENOMBREMENT_H
+
+void test_denombrement();
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include "TriDenombrement.h"
+#define DEBUG
+
+int main()
+{
+    /* code */
+
+    #ifdef DEBUG
+    test_denombrement();
+    #endif
+
+    return 0;
+}
